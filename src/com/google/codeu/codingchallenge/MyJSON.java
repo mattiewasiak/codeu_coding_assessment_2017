9f4aece96a7340ca213829cec2a08c2c1053f17b
mwasiak@mit.edu <mwasiak@mit.edu>
2017-04-16 13:49:05 -0400

    '4.16.2017'

--- src/com/google/codeu/codingchallenge/MyJSON.java
+++ src/com/google/codeu/codingchallenge/MyJSON.java
@@ -15,40 +15,64 @@
 package com.google.codeu.codingchallenge;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
 
 final class MyJSON implements JSON {
 
+  HashMap parsedData = new HashMap();
+	
   @Override
   public JSON getObject(String name) {
-    // TODO: implement this
-    return null;
+	  
+	
+    return (JSON) parsedData.get(name);
   }
 
   @Override
   public JSON setObject(String name, JSON value) {
-    // TODO: implement this
+    parsedData.put(name, value);
     return this;
   }
 
   @Override
   public String getString(String name) {
-    // TODO: implement this
-    return null;
+   
+    return (String)parsedData.get(name);
   }
 
   @Override
   public JSON setString(String name, String value) {
-    // TODO: implement this
+	parsedData.put(name, value);
     return this;
   }
 
   @Override
   public void getObjects(Collection<String> names) {
-    // TODO: implement this
+	 
+	  
+	  Set<String> keys = parsedData.keySet();
+	 
+	  for ( String key : keys) {
+		    
+		    
+		    if(parsedData.get(key) instanceof MyJSON){
+		    	
+		    	names.add(key);
+		    }
+		     
+		}
+	  
   }
 
   @Override
   public void getStrings(Collection<String> names) {
-    // TODO: implement this
+	  Set<String> keys = parsedData.keySet();
+	  for ( String key : keys) {
+		    if(parsedData.get(key) instanceof String){
+		    	names.add(key);
+		    }
+		     
+		}
   }
 }
--- src/com/google/codeu/codingchallenge/MyJSONParser.java
+++ src/com/google/codeu/codingchallenge/MyJSONParser.java
@@ -1,26 +1,231 @@
-// Copyright 2017 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.codeu.codingchallenge;
 
 import java.io.IOException;
 
-final class MyJSONParser implements JSONParser {
+final class MyJSONParser implements JSONParser{
+	
+	
 
-  @Override
-  public JSON parse(String in) throws IOException {
-    // TODO: implement this
-    return new MyJSON();
-  }
+	public int skipSpaces(String in, int startingIndex) throws IOException{
+		//returns the index of the next character not a space
+		
+		while(in.charAt(startingIndex) == ' '){
+			//ensure the end of the string hasn't been reached
+			if(startingIndex+1 > in.length()-1){
+				throw new IOException();
+			}
+			
+			startingIndex++;
+			
+		} 
+
+		return startingIndex;
+	}
+
+	
+
+	public int getStringEnd(String in, int currentChar) throws IOException{
+		
+		while(in.charAt(currentChar) != '"'){
+			
+			if(currentChar >= in.length()-1){
+				
+				throw new IOException();
+			}
+
+			if(in.charAt(currentChar) == '\\'){
+				if(currentChar  < in.length()){
+					
+					if(in.charAt(currentChar+1) == '\\' || in.charAt(currentChar+1) == 't' || in.charAt(currentChar+1) == 'n' || in.charAt(currentChar+1) == '"' ){
+						
+						currentChar++;
+					}
+					
+					else{
+						
+						throw new IOException();
+					}
+				}
+				
+			}
+			currentChar++;
+			
+
+
+		}
+
+		return currentChar;
+	}
+
+	@Override
+	public JSON parse(String in) throws IOException{
+
+		//should be returned
+		MyJSON parsed = new MyJSON();
+
+		
+		
+
+		//return occurs when a '}' is reached
+
+		//know the start is {
+		int currentChar = 0;
+		
+		currentChar = skipSpaces(in, 0);
+		
+		if(in.charAt(currentChar) == '{'){
+			currentChar = skipSpaces(in, currentChar+1);
+			
+			
+		}
+		else{
+			throw new IOException();
+		}
+		
+		
+		
+		
+		String lastChar = "{";
+		String key = "";
+		while(in.charAt(currentChar) != '}'){
+			
+			
+			
+			currentChar = skipSpaces(in, currentChar);
+			
+			//if lastChar is {, the next char has to be "
+			if(lastChar == "{" || lastChar == ","){
+				if(in.charAt(currentChar) != '"'){
+					
+					throw new IOException();
+				}
+				
+				else{
+					currentChar++;
+					//gather the key
+					int keyEnd = getStringEnd(in, currentChar);
+					
+					key = in.substring(currentChar, keyEnd);
+					
+					currentChar = keyEnd + 1;
+					
+					lastChar = "keyquote";
+					currentChar = skipSpaces(in, currentChar);
+					
+				}
+				
+			}
+
+			//if lastChar is keyquote, new char must be :
+			if(lastChar == "keyquote"){
+				if(in.charAt(currentChar) != ':'){
+					throw new IOException();
+				}
+				else{
+					lastChar = ":";
+					currentChar = skipSpaces(in, currentChar+1);
+
+				}
+			}
+
+			//after a colon should either be an object or a string
+			if(lastChar == ":"){
+				
+				
+				currentChar = skipSpaces(in, currentChar);
+				
+				
+				//string
+				if(in.charAt(currentChar) == '"'){
+					
+					int valueEnd = getStringEnd(in, currentChar+1);
+					
+					String value = in.substring(currentChar+1, valueEnd);
+					
+					
+					parsed.setString(key, value);
+					currentChar = valueEnd + 1;
+					lastChar = "valuequote";
+					currentChar = skipSpaces(in, currentChar);
+					
+					
+				}
+
+				//object
+				else if(in.charAt(currentChar) == '{'){
+					
+					
+					int foundEnd = currentChar;
+					while(in.charAt(foundEnd) != '}'){
+						if(foundEnd >= in.length()){
+							throw new IOException();
+						}
+						foundEnd++;
+					}
+					
+					parsed.setObject(key, parse(in.substring(currentChar, foundEnd+1)));
+					
+					if(foundEnd < in.length()-2){
+						currentChar = skipSpaces(in, foundEnd + 1);
+					}
+					else{
+						currentChar = foundEnd;
+					}
+					lastChar = "valuequote";
+					
+				}
+				else{
+					throw new IOException();
+				}
+			}
+
+			//if lastChar is valuequote, next char must be , (or }, but this won't go in the loop)
+			if(lastChar == "valuequote"){
+				
+				if(in.charAt(currentChar) != ',' && in.charAt(currentChar) != '}'){
+					
+					throw new IOException();
+
+				}
+				else if(in.charAt(currentChar) == '}'){
+					return parsed;
+				}
+
+				else{
+					lastChar = ",";
+					
+					if(currentChar < in.length() - 1){
+						
+						currentChar = skipSpaces(in, currentChar+1);
+					}
+					
+					
+
+				}
+
+
+
+			}
+			
+			if(currentChar >= in.length()-1){
+				throw new IOException();
+			}
+			
+
+
+
+
+
+
+			
+
+		}
+		
+		return parsed;
+
+
+	}
+
+
+
 }
